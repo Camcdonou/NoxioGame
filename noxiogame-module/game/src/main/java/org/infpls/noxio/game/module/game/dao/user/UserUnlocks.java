0d6858e0133d434681e3772c1c8a5557deac48c2
InfernoPlus <dmtinfernocide@gmail.com>
2020-04-12 03:43:17 -0500

    Tiny changes to unlocks.

--- noxiogame-module/game/src/main/java/org/infpls/noxio/game/module/game/dao/user/UserUnlocks.java
+++ noxiogame-module/game/src/main/java/org/infpls/noxio/game/module/game/dao/user/UserUnlocks.java
@@ -29,7 +29,7 @@ public class UserUnlocks {
     new Unlock(Key.CHAR_BLOCK, "Block", "In critical need of a good nights sleep.", Type.CHARACTER, 250, UserData.Type.SPEC, false),
     new Unlock(Key.CHAR_CARGO, "Cargo", "Punch.", Type.CHARACTER, 250, UserData.Type.SPEC, false),
     new Unlock(Key.CHAR_QUAD, "Quad", "Geometric concepts with swords.", Type.CHARACTER, 250, UserData.Type.SPEC, false),
-    new Unlock(Key.CHAR_INFERNO, "InfernoPlus", "Unsubscribed.", Type.CHARACTER, 123456789, UserData.Type.ADMIN, false),
+    new Unlock(Key.CHAR_INFERNO, "Plus", "Unsubscribed.", Type.CHARACTER, 123456789, UserData.Type.ADMIN, false),
     
     new Unlock(Key.ALT_BOXVO, "Talking Box", "Descriptions describe things.", Type.ALTERNATE, 100000, UserData.Type.FULL, false),
     new Unlock(Key.ALT_BOXRED, "Red Box", "Descriptions describe things.", Type.ALTERNATE, 100000, UserData.Type.FULL, false),
