33ded22213180755bd0aa822513d13ab472f063f
Camcdono <connorandrewmcdonough@gmail.com>
2025-12-19 11:27:09 -0500

    users may now use custom maps

--- noxiogame-module/game/src/main/java/org/infpls/noxio/game/module/game/util/Validation.java
+++ noxiogame-module/game/src/main/java/org/infpls/noxio/game/module/game/util/Validation.java
@@ -6,7 +6,11 @@ public class Validation {
   }
   
   public static boolean isAlphaNumericWithSpaces(final String in) {
-    return in.matches("[a-zA-Z0-9 ]*");
+    return in.matches("[a-zA-Z0-9 _-]*");
+  }
+  
+  public static boolean isMapName(final String in) {
+    return in.matches("[a-zA-Z0-9 _-]*");
   }
   
   /* If the string is longer than <int l> the the string is clipped to that length */
